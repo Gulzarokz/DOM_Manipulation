7a46c2e61168f9b2aa340d31e374dbd645e334ed
Gulzarokz <gokz22301@gamil.com>
2024-09-30 23:17:41 +0500

    Dom Manipulation practices

---
+++ style.js
@@ -0,0 +1,24 @@
+// var element;
+// element = document.all[8]; target document object by index
+// element = document.title;
+// element = document.body;
+// element = document.links;
+// element = document.links[1];
+// element = document.images[0];
+// element = document.URL;
+// element = document.domain;
+// element = document.baseURI;
+
+
+// var element;
+// element = document.getElementById("header");
+// element = document.getElementById("menu");
+// element = document.getElementById("footer");
+// element = document.getElementById("sidebar");
+// element = document.getElementsByClassName("list")[0];
+// element = document.getElementsByClassName("list")[2];
+
+
+// element = document.getElementsByTagName("ul")
+
+// console.log(element);
